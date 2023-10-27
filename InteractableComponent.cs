00398841533fc70a9f203e97c24de329b0803344
VandamusHeelins <149211857+VandamusHeelins@users.noreply.github.com>
2023-10-28 00:45:20 +0100

    Add files via upload

---
+++ InteractableComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.PirateCrew.Components
+{
+	public class InteractableComponent: MonoBehaviour
+	{
+		[SerializeField] private UnityEvent _action;
+		public void Iteract()
+		{
+			_action?.Invoke();
+		}
+	}
+}
